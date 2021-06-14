cf5d3affc949378e9d3a8ee8706cc0c332d1ada3
gidon <gd890121@naver.com>
2021-06-14 17:59:58 +0900

    basic_table

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from table import views
+from table.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('table/', include('table.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index'),  # '/' 에 해당되는 path
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
