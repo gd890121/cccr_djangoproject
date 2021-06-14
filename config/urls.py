1a2fc873e2a9b848246e01119f62f8bd73be4930
gidon <gd890121@naver.com>
2021-06-14 16:01:00 +0900

    save commit

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from table import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('table/', include('table.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),  # '/' 에 해당되는 path
 ]
