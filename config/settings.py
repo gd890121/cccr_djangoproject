dc77850e900a8a44783016796b61790e96e6e9be
gidon <gd890121@naver.com>
2021-06-15 20:56:32 +0900

    finalsidebar

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'data.apps.DataConfig',
+    'pybo.apps.PyboConfig',
     'common.apps.CommonConfig',
     'table.apps.TableConfig',
     'django.contrib.admin',
@@ -105,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from table.views import base_views
+from pybo.views import base_views
+from data.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('table/', include('table.urls')),
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
+    path('pybo/', include('pybo.urls')),
+    path('data/', include('data.urls')),
 ]
